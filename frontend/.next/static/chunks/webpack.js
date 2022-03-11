e481bdaf37274f32bcd657a735d48c1e3d029c97
Preston Carlton <prescarlton@gmail.com>
2022-03-10 21:19:25 -0500

    removed example content, added project types

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "6e8560d21675ccf8f007"; }
+/******/ 		__webpack_require__.h = function() { return "c252c1b2f03bd984c7af"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
